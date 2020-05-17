e713e6232b8f87d6768533e72718358c3f3cb460
idat50me <takine1001@gmail.com>
2020-05-17 14:55:20 +0900

    includeの形式を変える2

--- test/binpow.test.cpp
+++ test/binpow.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/all/NTL_1_B"
 #include "../../lib1/include_files.hpp"
-#include "../../lib1/alias_declaration.hpp"
+#include "../lib1/alias_declaration.hpp"
 #include "../lib1/macros.hpp"
 #include "../binpow.hpp"
 
