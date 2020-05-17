9a7cb013e0363803c0f1f83b8f3e2cb4c1536059
idat50me <takine1001@gmail.com>
2020-05-17 15:22:47 +0900

    fix

--- test/binpow.test.cpp
+++ test/binpow.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/all/NTL_1_B"
 #include "lib1/include_files.cpp"
-#include "lib1/alias_declaration.hpp"
-#include "lib1/macros.hpp"
+#include "lib1/alias_declaration.cpp"
+#include "lib1/macros.cpp"
 #include "lib1/binpow.cpp"
 
 int main() {
