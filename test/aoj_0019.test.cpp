d2cb7145d7436663aa17d6147c4e46bd6a1d885a
idat50me <takine1001@gmail.com>
2020-06-25 22:54:25 +0900

    fix URL of aoj_0019.test.cpp

--- test/aoj_0019.test.cpp
+++ test/aoj_0019.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/0019"
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0019"
 
 #ifndef call_include
 #define call_include
