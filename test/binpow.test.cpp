5a6a90588e6e4b2bab2c4bce433f6fbb2e4917b6
idat50me <takine1001@gmail.com>
2020-06-25 15:58:45 +0900

    modified binpow.test.cpp

--- test/binpow.test.cpp
+++ test/binpow.test.cpp
@@ -1,8 +1,8 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/all/NTL_1_B"
-#include "lib1/include_files.cpp"
-#include "lib1/alias_declaration.cpp"
-#include "lib1/macros.cpp"
-#include "lib1/binpow.cpp"
+#include "../lib1/include_files.cpp"
+#include "../lib1/alias_declaration.cpp"
+#include "../lib1/macros.cpp"
+#include "../lib1/binpow.cpp"
 
 int main() {
 	int m,n; cin>>m>>n;
