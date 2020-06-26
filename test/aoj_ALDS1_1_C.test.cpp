1ec2d9696d9f43434816b72dc4c78e0a2bb82804
idat50me <takine1001@gmail.com>
2020-06-26 22:21:23 +0900

    add isprime

---
+++ test/aoj_ALDS1_1_C.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_1_C"
+
+#ifndef call_include
+#define call_include
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include "pre/macros.cpp"
+#include "math/isprime.cpp"
+
+int main() {
+	Sieve sv(10010);
+	int n; cin>>n;
+	int res=0;
+	for(int i=0; i<n; i++) {
+		int a; cin>>a;
+		if(sv.isprime(a)) res++;
+	}
+	cout<<res<<endl;
+}
