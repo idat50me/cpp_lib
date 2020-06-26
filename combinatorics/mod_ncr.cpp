952d3e890014621b1131f2bc185c0125dc5d19a9
idat50me <takine1001@gmail.com>
2020-06-26 18:06:56 +0900

    fix
    extern

--- combinatorics/mod_ncr.cpp
+++ combinatorics/mod_ncr.cpp
@@ -10,6 +10,9 @@ using namespace std;
 #include "pre/macros.cpp"
 #include "combinatorics/pre_mod_comb.cpp"
 
+extern vector<ll> fact__,inv__,finv__;
+extern ll mod_comb_p__;
+
 ll mod_nCr(ll n, ll r) {
 	ll p=mod_comb_p__;
 	if (r > n) return 0;
