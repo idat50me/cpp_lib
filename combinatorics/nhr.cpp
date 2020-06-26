eb0a28316a253284c06ce1f162cc9bc41f38b426
idat50me <takine1001@gmail.com>
2020-06-26 21:55:37 +0900

    fix yuki_0117

--- combinatorics/nhr.cpp
+++ combinatorics/nhr.cpp
@@ -12,11 +12,13 @@ using namespace std;
 #include "combinatorics/mod_ncr.cpp"
 
 ll nHr(ll n, ll r) {
+	if(r==0) return 1;
 	if(n==0) return 0;
 	return nCr(n+r-1,n-1);
 }
 
 ll nHr(ll n, ll r, ll p, vector<ll> &fact, vector<ll> &inv, vector<ll> &finv) {
+	if(r==0) return 1;
 	if(n==0) return 0;
 	return mod_nCr(n+r-1,n-1,p,fact,inv,finv);
 }
