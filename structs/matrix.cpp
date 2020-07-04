83bcf85b02f12ff4e8daff0cc6a940533399ac78
idat50me <takine1001@gmail.com>
2020-07-04 13:56:46 +0900

    fix matrix.cpp
    初期化処理をやめて make() に統一

--- structs/matrix.cpp
+++ structs/matrix.cpp
@@ -10,38 +10,30 @@ using namespace std;
 template<class T>
 class Matrix{
 	vector<vector<T>> mat;
-	int height, width;
 
 public:
 
-	Matrix(int h=1, int w=1) : height(h), width(w), mat(h,vector<T>(w)) {}
-	Matrix(int h, int w, T n) : height(h), width(w), mat(h,vector<T>(w,n)) {}
-
-	void resize(int h, int w) {
-		height=h, width=w;
-		mat.resize(h,vector<T>(w));
-	}
-	void resize(int h, int w, T n) {
-		height=h, width=w;
-		mat.resize(h,vector<T>(w,n));
-	}
+	void make(int h, int w, T n=0) {
+		mat=vector<vector<T>>(h,vector<T>(w,n));
+	};
 
 	void in() {
-		for(int i=0; i<mat.height; i++) for(int j=0; j<mat.width; j++) {
+		for(int i=0; i<mat.size(); i++) for(int j=0; j<mat[i].size(); j++) {
 			cin>>mat[i][j];
 		}
 	}
 
 	void out() {
-		for(int i=0; i<mat.height; i++) {
-			for(int j=0; j<mat.width; j++) {
+		for(int i=0; i<mat.size(); i++) {
+			for(int j=0; j<mat[i].size(); j++) {
 				cout<<mat[i][j]<<" ";
 			}
 			cout<<endl;
 		}
 	}
 
-	inline vector<T>& operator[](size_t idx) {
+	inline vector<T> &operator[](int idx) {
+		assert(0<=idx && idx<mat.size());
 		return mat[idx];
 	}
 };
