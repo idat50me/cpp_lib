07ed448092d96510b26668f4de2d3840700b4af1
idat50me <takine1001@gmail.com>
2020-07-13 07:34:19 +0900

    add yuki_0888.test.cpp

---
+++ test/yuki_0888.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://yukicoder.me/problems/no/888"
+
+#ifndef call_include
+#define call_include
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include "pre/macros.cpp"
+#include "math/divisor.cpp"
+
+int main() {
+	ll N; cin>>N;
+	vector<ll> v=divisor(N);
+	ll res=0;
+
+	for(int i=0; i<v.size(); i++) res+=v[i];
+	cout<<res<<endl;
+}
