fdb4c1c4928d300cec5c22cbe2bae85ba6784983
idat50me <takine1001@gmail.com>
2020-07-14 18:16:24 +0900

    add testfile for verify ncr2.cpp

---
+++ test/aoj_DPL_5_E.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/DPL_5_E"
+
+#ifndef call_include
+#define call_include
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include "pre/macros.cpp"
+#include "combinatorics/ncr2.cpp"
+
+int main() {
+	int n,k; cin>>n>>k;
+	nCr2 table(n,k,MPRIME);
+	cout<<table.calc(n,k)<<endl;
+}
