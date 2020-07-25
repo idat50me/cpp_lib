dd2388a725e4ddfe1efa5c7114e27fafb2f116b7
idat50me <takine1001@gmail.com>
2020-07-26 01:53:03 +0900

    add aoj_DSL_2_A

---
+++ test/aoj_DSL_2_A.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_A"
+
+#ifndef call_include
+#define call_include
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include "../tree/segtree_RMQ.cpp"
+
+int main() {
+	int N,Q; cin>>N>>Q;
+	vector<int> v(N,(1<<31)-1);
+	RMQ rmq_tree(v);
+
+	for(int i=0; i<Q; i++) {
+		int com,x,y; cin>>com>>x>>y;
+
+		if(com==0) rmq_tree.update(x,y);
+		else cout<<rmq_tree.getmin(x,y+1)<<'\n';
+	}
+	cout<<flush;
+}
