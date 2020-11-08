edab2f9aea9b4cad2f7e8c2f70fdfb903b1c512e
idat50me <takine1001@gmail.com>
2020-11-09 01:02:50 +0900

    add yosupo_rangesum

---
+++ test/yosupo_static_range_sum.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_sum"
+
+#ifndef call_include
+#define call_include
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include "tree/segtree.cpp"
+using ll = long long;
+
+ll sum_op(ll a, ll b) {
+	return a+b;
+}
+
+ll e() {
+	return 0LL;
+}
+
+int main() {
+	int N,Q; cin>>N>>Q;
+	vector<ll> v(N);
+	for(int i=0; i<N; i++) cin>>v[i];
+	segtree<ll,sum_op,e> tree(v);
+
+	for(int i=0; i<Q; i++) {
+		int l,r; cin>>l>>r;
+		cout<<tree.get(l,r)<<'\n';
+	}
+	cout<<flush;
+}
