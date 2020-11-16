76cc3bc66b8e8600bcb7fc81da5e3405c2289e32
idat50me <59227194+idat50me@users.noreply.github.com>
2020-11-16 19:42:04 +0900

    update unionfind mergetech

--- graph/union_find.cpp
+++ graph/union_find.cpp
@@ -14,16 +14,16 @@ private:
 
 public:
 	UnionFind(int n) : parent(n),num(n,1) {
-		treenum=n;
+		treenum = n;
 		for(int i=0; i<n; i++) {
-			parent[i]=i;
+			parent[i] = i;
 		}
 	}
 
 	int root(int x) {
 		assert(x < parent.size());
-		if(parent[x]==x) return x;
-		return parent[x]=root(parent[x]);
+		if(parent[x] == x) return x;
+		return parent[x] = root(parent[x]);
 	}
 
 	int size(int x) {
@@ -31,17 +31,17 @@ public:
 	}
 
 	void merge(int x, int y) {
-		int xrt=root(x);
-		int yrt=root(y);
-		if(xrt==yrt) return;
-		if(xrt < yrt) swap(xrt,yrt);
-		parent[yrt]=xrt;
-		num[xrt]+=num[yrt];
+		int xrt = root(x);
+		int yrt = root(y);
+		if(xrt == yrt) return;
+		if(size(xrt) < size(yrt)) swap(xrt,yrt);
+		parent[yrt] = xrt;
+		num[xrt] += num[yrt];
 		treenum--;
 	}
 
 	bool same(int x, int y) {
-		return root(x)==root(y);
+		return root(x) == root(y);
 	}
 
 	int tnum() {
