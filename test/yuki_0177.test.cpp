93206200ca017c110443057503c7ca3fbb6ea61f
idat50me <59227194+idat50me@users.noreply.github.com>
2020-12-10 11:08:29 +0900

    add yuki_0177

---
+++ test/yuki_0177.test.cpp
@@ -0,0 +1,42 @@
+#define PROBLEM "https://yukicoder.me/problems/177"
+
+#ifndef call_include
+#define call_include
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include "graph/maxflow.cpp"
+#include "structs/matrix.cpp"
+
+int main() {
+	int W,N,M;
+	vector<int> J,C;
+	matrix<bool> X;
+	cin>>W>>N;
+	J.resize(N);
+	for(int &j: J) cin>>j;
+	cin>>M;
+	C.resize(M);
+	for(int &c: C) cin>>c;
+	X.resize(N,M,true);
+	for(int i=0; i<M; i++) {
+		int Q; cin>>Q;
+		for(int j=0; j<Q; j++) {
+			int x; cin>>x;
+			X[--x][i] = false;
+		}
+	}
+
+	maxflow mf(N+M+2);
+	for(int i=0; i<N; i++) {
+		mf.add(N+M, i, J[i]);
+		for(int j=0; j<M; j++) {
+			if(X[i][j]) mf.add(i, N+j, (1LL<<62)-1);
+		}
+	}
+	for(int i=0; i<M; i++) mf.add(N+i, N+M+1, C[i]);
+
+	if(W <= mf.solve(N+M, N+M+1)) cout<<"SHIROBAKO"<<endl;
+	else cout<<"BANSAKUTSUKITA"<<endl;
+}
