e5028eeead1e27f6316e881ef37ca64b7c89fef8
idat50me <59227194+idat50me@users.noreply.github.com>
2020-12-11 02:16:40 +0900

    update maxflow

--- graph/maxflow.cpp
+++ graph/maxflow.cpp
@@ -18,10 +18,11 @@ private:
 
 	const int vnum;
 	vector<vector<edge>> G;
-	vector<bool> used;
+	vector<int> used;
+	int ts;
 
 public:
-	maxflow(int V) : vnum(V), G(V), used(V) {}
+	maxflow(int V) : vnum(V), G(V), used(V,-1), ts(0) {}
 
 	void add(int from, int to, long long cap) {
 		G[from].push_back(edge(to, G[to].size(), cap));
@@ -31,9 +32,9 @@ public:
 private:
 	long long dfs(int s, int t, long long flow) {
 		if(s == t) return flow;
-		used[s] = true;
+		used[s] = ts;
 		for(edge &ed : G[s]) {
-			if(!used[ed.next] && ed.cap>0) {
+			if(used[ed.next]!=ts && ed.cap>0) {
 				long long captmp = dfs(ed.next, t, min(flow,ed.cap));
 				if(captmp > 0) {
 					ed.cap -= captmp;
@@ -49,7 +50,6 @@ public:
 	long long solve(int s, int t) {
 		long long res = 0;
 		while(1) {
-			used.assign(vnum, false);
 			long long restmp = dfs(s, t, (1LL<<62)-1);
 			if(restmp == 0) return res;
 			res += restmp;
