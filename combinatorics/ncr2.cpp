974b4bc2addb1299e8313e5ea94967ebac86d55e
idat50me <59227194+idat50me@users.noreply.github.com>
2020-12-11 23:39:25 +0900

    fix ncr2

--- combinatorics/ncr2.cpp
+++ combinatorics/ncr2.cpp
@@ -12,9 +12,7 @@ private:
 	long long P;
 
 	void calc_inv(long long r) {
-		inv.resize(r+1);
-		finv.resize(r+1);
-		inv[1] = finv[0] = finv[1]=1LL;
+		inv[1] = finv[0] = finv[1] = 1LL;
 		for(int i=2; i<=r; i++) {
 			inv[i] = P-inv[P%i]*(P/i)%P;
 			finv[i] = finv[i-1]*inv[i]%P;
@@ -22,20 +20,17 @@ private:
 	}
 
 public:
-	NCR(long long n, long long r, long long p) {
+	NCR(long long n, long long r, long long p) : comb(n+1), inv(r+1), finv(r+1), P(p) {
 		if(n/2 < r) r = n/2;
-		P=p;
 		calc_inv(r);
 
-		comb.resize(r+1);
-		comb[0]=1;
+		comb[0] = 1;
 		for(int i=1; i<=r; i++) {
 			comb[i] = comb[i-1]*(n-i+1)%P*inv[i]%P;
 		}
 	}
 
-	NCR(long long r, long long p) {
-		P=p;
+	NCR(long long r, long long p) : inv(r+1), finv(r+1), P(p) {
 		calc_inv(r);
 	}
 
