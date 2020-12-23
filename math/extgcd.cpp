344409e5b4f29e40186a5e3a3322779c606d5bf1
idat50me <59227194+idat50me@users.noreply.github.com>
2020-12-23 14:23:22 +0900

    update extgcd

--- math/extgcd.cpp
+++ math/extgcd.cpp
@@ -7,18 +7,33 @@ using namespace std;
 #endif
 
 long long extgcd(long long a, long long b, long long &x, long long &y) {
-	if(b == 0) {
-		x = 1; y = 0;
-		return a;
+	long long x_ = a, xd = 1, xdd = 0,
+			  y_ = b, yd = 0, ydd = 1,
+			  div;
+	while(true) {
+		if(!y_) {
+			x = xd; y = xdd;
+			return x_;
+		}
+		div = x_/y_;
+		x_  -= div*y_;
+		xd  -= div*yd;
+		xdd -= div*ydd;
+
+		if(!x_) {
+			x = yd; y = ydd;
+			return y_;
+		}
+		div = y_/x_;
+		y_  -= div*x_;
+		yd  -= div*xd;
+		ydd -= div*xdd;
 	}
-	long long d = extgcd(b, a%b, y, x);
-	y -= a/b*x;
-	return d;
 }
 
 long long extgcd(long long a, long long b, long long c, long long &x, long long &y) {
 	long long d = extgcd(a, b, x, y);
-	if(c%d) return -1;
+	if(c % d) return -1;
 	x *= c/d;
 	y *= c/d;
 	return d;
