56a55a811c39bd312774a6ea9040aaed5d0ec616
idat50me <59227194+idat50me@users.noreply.github.com>
2020-12-23 18:12:52 +0900

    add aoj_ALDS1_1_B

---
+++ test/aoj_ALDS1_1_B.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_1_B"
+
+#ifndef call_include
+#define call_include
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include "math/extgcd.cpp"
+
+int main() {
+	int x,y; cin>>x>>y;
+	long long z;
+	cout<<extgcd(x,y,z,z)<<endl;
+}
