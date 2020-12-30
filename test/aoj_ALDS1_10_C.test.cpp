c12f7448f386dd4e169b029c1377962dc01f1f51
idat50me <59227194+idat50me@users.noreply.github.com>
2020-12-30 23:28:14 +0900

    add lcs

---
+++ test/aoj_ALDS1_10_C.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_10_C"
+
+#ifndef call_include
+#define call_include
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include "dp/lcs.cpp"
+
+int main() {
+	int Q; cin>>Q;
+	while(Q--) {
+		string X,Y; cin>>X>>Y;
+		cout<<LCS(X,Y)<<endl;
+	}
+}
