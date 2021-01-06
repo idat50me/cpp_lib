963693289fe893e94cebbd5f19a74cd5e9357b2b
idat50me <59227194+idat50me@users.noreply.github.com>
2021-01-07 08:32:51 +0900

    add z_algorithm

---
+++ string/z_algorithm.cpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifndef call_include
+#define call_include
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+vector<int> z_algorithm(const string &S) {
+	int N = S.length();
+	vector<int> res(N, 0);
+
+	for(int i=1, c=0; i<N; i++) {
+		int l = i-c;
+		if(i+res[l] < c+res[c]) {
+			res[i] = res[l];
+		}
+		else {
+			int l = max(c+res[c]-i, 0);
+			while(i+l<N && S[l]==S[i+l]) l++;
+			res[i] = l;
+			c = i;
+		}
+	}
+
+	res[0] = N;
+	return res;
+}
---
+++ test/yosupo_zalgorithm.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/zalgorithm"
+
+#ifndef call_include
+#define call_include
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include "string/z_algorithm.cpp"
+
+int main() {
+	string S; cin>>S;
+	vector<int> v = z_algorithm(S);
+	for(int i=0; i<v.size(); i++) cout<<v[i]<<(i==v.size()-1?'\n':' ');
+}
