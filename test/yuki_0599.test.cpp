4167f81452ec3a4eca5d35a3e552254a1b2ca1c8
idat50me <59227194+idat50me@users.noreply.github.com>
2021-01-08 23:33:11 +0900

    add yuki_0559

---
+++ test/yuki_0599.test.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://yukicoder.me/problems/no/599"
+
+#ifndef call_include
+#define call_include
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include "string/z_algorithm.cpp"
+#include "mod/modint.cpp"
+
+using mi = mint<1000000007>;
+
+int main() {
+	string S; cin>>S;
+	vector<mi> dp(S.length()/2+1, 0);
+	dp[0] = 1;
+
+	for(int i=0; i<=(S.length()-1)/2; i++) {
+		vector<int> v = z_algorithm(string(S.begin()+i, S.end()-i));
+		for(int j=1; j<=v.size()/2; j++) {
+			if(v[v.size()-j]==j) dp[i+j] += dp[i];
+		}
+	}
+
+	cout<<accumulate(dp.begin(), dp.end(), mi(0))<<endl;
+}
