732c4bda32c31b692e8a7283561bf349d7754eb3
idat50me <59227194+idat50me@users.noreply.github.com>
2021-01-26 14:43:55 +0900

    update divisor

--- math/divisor.cpp
+++ math/divisor.cpp
@@ -11,16 +11,19 @@ using namespace std;
 
 
 vector<long long> divisor(long long x) {
-	vector<long long> res;
-	long long i = 1;
-	for( ; i*i < x; i++) {
-		if(x%i) continue;
-		res.emplace_back(i);
-		res.emplace_back(x/i);
+	vector<long long> res = {1};
+	long long i = 2, id = 0, sz;
+	while(i*i<=x || i==x) {
+		if(x%i) { i++; id=0; continue; }
+		sz = res.size();
+		for( ; id<sz; id++) res.emplace_back(res[id]*i);
+		x /= i;
 	}
-	if(i*i==x)
-		res.emplace_back(i);
-	
+	if(x > 1) {
+		sz = res.size();
+		for(int j=0; j<sz; j++) res.emplace_back(res[j]*x);
+	}
+
 	sort(res.begin(),res.end());
 	return res;
 }
