3c2288ee7a305288beb3ae3e43bf848ddbf89ff2
idat50me <59227194+idat50me@users.noreply.github.com>
2021-01-30 17:51:50 +0900

    add aoj_2932

---
+++ aoj_2932.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/2932"
+
+#ifndef call_include
+#define call_include
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include "math/primefact.cpp"
+
+using ll = long long;
+
+int main() {
+	ll N; cin>>N;
+	vector<pair<ll,int>> v=primefact(N);
+
+	ll mx=1;
+	for(auto &p: v) mx *= p.second+1;
+
+	cout <<v.size()<<" "<<mx-1<<endl;
+}
