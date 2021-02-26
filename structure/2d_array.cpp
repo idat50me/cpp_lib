dc31784c65e9d2b0f2ea74448fcd513fccda7d62
idat50me <59227194+idat50me@users.noreply.github.com>
2021-02-26 21:39:59 +0900

    update: v2d
    rangeout() 追加

--- structure/2d_array.cpp
+++ structure/2d_array.cpp
@@ -6,8 +6,7 @@
 using namespace std;
 #endif
 
-template<typename T>
-struct v2d {
+template<typename T> struct v2d {
 private:
 	vector<vector<T>> m;
 
@@ -17,15 +16,20 @@ public:
 	v2d(int h, int w, const T &init) : m(h, vector<T>(w, init)) {}
 	v2d(const initializer_list<initializer_list<T>> m_init) : m(m_init.begin(), m_init.end()) {}
 
-	void assign(int h, int w) { m.assign(h, vector<T>(w)); }
-	void assign(int h, int w, const T init) { m.assign(h, vector<T>(w, init)); }
+	void assign(int h, int w) {
+		m.assign(h, vector<T>(w));
+	}
+	void assign(int h, int w, const T init) {
+		m.assign(h, vector<T>(w, init));
+	}
 
 	inline int size() const {
 		return m.size();
 	}
 
 	void in() {
-		for(vector<T> &v: m) for(T &val: v) cin>>val;
+		for(vector<T> &v : m)
+			for(T &val : v) cin >> val;
 	}
 	void in(int h, int w) {
 		m.resize(h, vector<T>(w));
@@ -34,17 +38,22 @@ public:
 
 	void out() {
 		int h = m.size();
-		for(vector<T> &v: m) {
+		for(vector<T> &v : m) {
 			int sz = v.size();
-			for(int j=0; j<sz; j++) {
-				cout<<v[j]<<(j==sz-1 ? '\n' : ' ');
+			for(int j = 0; j < sz; j++) {
+				cout << v[j] << (j == sz - 1 ? '\n' : ' ');
 			}
 		}
-		cout<<flush;
+		cout << flush;
 	}
 
 	inline vector<T> &operator[](int idx) {
-		assert(0<=idx && idx<m.size());
+		assert(0 <= idx && idx < m.size());
 		return m[idx];
+	}
+
+	bool rangeout(int x, int y) {
+		if(x < 0 || y < 0 || x >= size() || y >= m[x].size()) return true;
+		return false;
 	}
 };
--- structure/docs/2d_array.md
+++ structure/docs/2d_array.md
@@ -20,6 +20,7 @@ documentation_of: ../2d_array.cpp
 - `in(h, w)`：`h` 行 `w` 列の $2$ 次元配列を作り標準入力する．
 - `out()`：要素を空白区切りで標準出力する．
 - `operator[](idx)`：`idx` 番目の要素である $1$ 次元配列の参照を返す．`vector` を用いた多次元配列と同様に扱える．
+- `rangeout(x ,y)`：`x` 行 `y` 列目の要素が存在しない場合は `true` ，存在する場合は `false` を返す．
 
 ## 計算量
 $H$ 行 $W$ 列の行列を扱うとする．
@@ -29,6 +30,7 @@ $H$ 行 $W$ 列の行列を扱うとする．
 - `in()`：$O(HW)$
 - `out()`：$O(HW)$
 - `operator[](idx)`：$O(1)$
+- `rangeout(x, y)`：$O(1)$
 
 ## 参考
 - [初期化子リスト - cpprefjp C++日本語リファレンス](https://cpprefjp.github.io/lang/cpp11/initializer_lists.html)
