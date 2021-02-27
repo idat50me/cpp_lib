9831ba6b4ce55a67c04d0e1703ce148b57432b81
idat50me <59227194+idat50me@users.noreply.github.com>
2021-02-28 01:32:23 +0900

    fix: extgcd
    a < 0 or b < 0 のとき gcd(|a|, |b|) を返し，x, y の符号変換の処理を追加

--- math/extgcd.cpp
+++ math/extgcd.cpp
@@ -7,34 +7,36 @@ using namespace std;
 #endif
 
 long long extgcd(long long a, long long b, long long &x, long long &y) {
-	long long x_ = a, xd = 1, xdd = 0,
-	          y_ = b, yd = 0, ydd = 1,
-	          div;
+	long long x_ = abs(a), xd = 1, xdd = 0, y_ = abs(b), yd = 0, ydd = 1, div;
 	while(true) {
 		if(!y_) {
-			x = xd; y = xdd;
+			x = xd;
+			y = xdd;
 			return x_;
 		}
-		div = x_/y_;
-		x_  -= div*y_;
-		xd  -= div*yd;
-		xdd -= div*ydd;
+		div = x_ / y_;
+		x_ -= div * y_;
+		xd -= div * yd;
+		xdd -= div * ydd;
 
 		if(!x_) {
-			x = yd; y = ydd;
+			x = yd;
+			y = ydd;
 			return y_;
 		}
-		div = y_/x_;
-		y_  -= div*x_;
-		yd  -= div*xd;
-		ydd -= div*xdd;
+		div = y_ / x_;
+		y_ -= div * x_;
+		yd -= div * xd;
+		ydd -= div * xdd;
 	}
 }
 
 long long extgcd(long long a, long long b, long long c, long long &x, long long &y) {
 	long long d = extgcd(a, b, x, y);
 	if(c % d) return -1;
-	x *= c/d;
-	y *= c/d;
+	x *= c / d;
+	if(a < 0) x = -x;
+	y *= c / d;
+	if(b < 0) y = -y;
 	return d;
 }
