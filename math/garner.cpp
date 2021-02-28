195477668f54db9926809aafe79d2d4bc2f55a62
idat50me <59227194+idat50me@users.noreply.github.com>
2021-03-01 01:43:03 +0900

    fix: garner

--- math/garner.cpp
+++ math/garner.cpp
@@ -8,47 +8,47 @@ using namespace std;
 
 #include "../math/extgcd.cpp"
 
-long long pregarner(vector<long long> &b, vector<long long> &m, const int p) {
+long long pregarner(vector<long long> &B, vector<long long> &M, const int p) {
 	long long lcm = 1, g, gi, gj;
-	for(int i = 0; i < m.size(); i++) {
-		for(int j = i + 1; j < m.size(); j++) {
-			g = gcd(m[i], m[j]);
-			if((b[j] - b[i]) % g) return -1;
+	for(int i = 0; i < M.size(); i++) {
+		for(int j = i + 1; j < M.size(); j++) {
+			g = gcd(M[i], M[j]);
+			if((B[j] - B[i]) % g) return -1;
 
-			m[i] /= g, m[j] /= g;
-			gi = gcd(m[i], g), gj = g / gi;
+			M[i] /= g, M[j] /= g;
+			gi = gcd(M[i], g), gj = g / gi;
 
 			do {
 				g = gcd(gi, gj);
 				gi *= g, gj /= g;
 			} while(g > 1);
 
-			m[i] *= gi, m[j] *= gj;
-			b[i] %= m[i], b[j] %= m[j];
+			M[i] *= gi, M[j] *= gj;
+			B[i] %= M[i], B[j] %= M[j];
 		}
-		(lcm *= m[i]) %= p;
+		(lcm *= M[i]) %= p;
 	}
 	return lcm;
 }
 
-long long garner(const vector<long long> &b, const vector<long long> &m, const int p) {
-	const int n = m.size();
+long long garner(const vector<long long> &B, const vector<long long> &M, const int p) {
+	const int n = M.size();
 	vector<long long> mprod(n + 1, 1);
 	vector<long long> X(n + 1, 0);
 	long long t, x, y, inv;
 
 	for(int k = 0; k < n; k++) {
-		if(extgcd(mprod[k], m[k], 1, x, y) == -1) return -1;
-		inv = x % m[k];
-		if(inv < 0) inv += m[k];
-		t = (b[k] - X[k]) * inv % m[k];
-		if(t < 0) t += m[k];
+		if(extgcd(mprod[k], M[k], 1, x, y) == -1) return -1;
+		inv = x % M[k];
+		if(inv < 0) inv += M[k];
+		t = (B[k] - X[k]) * inv % M[k];
+		if(t < 0) t += M[k];
 		for(int i = k + 1; i < n; i++) {
-			(X[i] += t * mprod[i]) %= m[i];
-			(mprod[i] *= m[k]) %= m[i];
+			(X[i] += t * mprod[i]) %= M[i];
+			(mprod[i] *= M[k]) %= M[i];
 		}
 		(X[n] += t * mprod[n]) %= p;
-		(mprod[n] *= m[k]) %= p;
+		(mprod[n] *= M[k]) %= p;
 	}
 	return X.back();
 }
