a65e9a246266b50693624d77060b1fd274350fc6
idat50me <59227194+idat50me@users.noreply.github.com>
2021-02-28 15:24:55 +0900

    fix: yuki_0177
    URLの表記を統一（*/problems/no/problemno）

--- test/yuki_0177.test.cpp
+++ test/yuki_0177.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://yukicoder.me/problems/177"
+#define PROBLEM "https://yukicoder.me/problems/no/177"
 
 #ifndef call_include
 #define call_include
@@ -10,33 +10,36 @@ using namespace std;
 #include "structure/2d_array.cpp"
 
 int main() {
-	int W,N,M;
-	vector<int> J,C;
+	int W, N, M;
+	vector<int> J, C;
 	v2d<bool> X;
-	cin>>W>>N;
+	cin >> W >> N;
 	J.resize(N);
-	for(int &j: J) cin>>j;
-	cin>>M;
+	for(int &j : J) cin >> j;
+	cin >> M;
 	C.resize(M);
-	for(int &c: C) cin>>c;
-	X.assign(N,M,true);
-	for(int i=0; i<M; i++) {
-		int Q; cin>>Q;
-		for(int j=0; j<Q; j++) {
-			int x; cin>>x;
+	for(int &c : C) cin >> c;
+	X.assign(N, M, true);
+	for(int i = 0; i < M; i++) {
+		int Q;
+		cin >> Q;
+		for(int j = 0; j < Q; j++) {
+			int x;
+			cin >> x;
 			X[--x][i] = false;
 		}
 	}
 
-	maxflow mf(N+M+2);
-	for(int i=0; i<N; i++) {
-		mf.add(N+M, i, J[i]);
-		for(int j=0; j<M; j++) {
-			if(X[i][j]) mf.add(i, N+j, (1LL<<62)-1);
+	maxflow mf(N + M + 2);
+	for(int i = 0; i < N; i++) {
+		mf.add(N + M, i, J[i]);
+		for(int j = 0; j < M; j++) {
+			if(X[i][j]) mf.add(i, N + j, (1LL << 62) - 1);
 		}
 	}
-	for(int i=0; i<M; i++) mf.add(N+i, N+M+1, C[i]);
+	for(int i = 0; i < M; i++) mf.add(N + i, N + M + 1, C[i]);
 
-	if(W <= mf.solve(N+M, N+M+1)) cout<<"SHIROBAKO"<<endl;
-	else cout<<"BANSAKUTSUKITA"<<endl;
+	if(W <= mf.solve(N + M, N + M + 1)) cout << "SHIROBAKO" << endl;
+	else
+		cout << "BANSAKUTSUKITA" << endl;
 }
