903e07a4c0ab2ff901d425561745eb7572ee5f95
idat50me <59227194+idat50me@users.noreply.github.com>
2021-03-01 01:11:17 +0900

    fix: yuki_0187

--- test/yuki_0187.test.cpp
+++ test/yuki_0187.test.cpp
@@ -13,11 +13,15 @@ constexpr int MPRIME = 1000000007;
 int main() {
 	int N;
 	vector<long long> X, Y;
+	bool zr = true;
 
 	cin >> N;
 	X.resize(N);
 	Y.resize(N);
-	for(int i = 0; i < N; i++) cin >> X[i] >> Y[i];
+	for(int i = 0; i < N; i++) {
+		cin >> X[i] >> Y[i];
+		if(X[i]) zr = false;
+	}
 
 	long long lcm = pregarner(X, Y, MPRIME);
 	if(lcm == -1) {
@@ -26,7 +30,7 @@ int main() {
 	}
 
 	long long ans = garner(X, Y, MPRIME);
-	if(ans == 0) cout << lcm << endl;
+	if(zr) cout << lcm << endl;
 	else
 		cout << ans << endl;
 }
