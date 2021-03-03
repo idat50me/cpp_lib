e3041147f59ded25480f699001e7b16c7416efe7
idat50me <59227194+idat50me@users.noreply.github.com>
2021-03-04 00:31:12 +0900

    update: doubling
    yuki_1013 が通るようになった

--- dp/doubling.cpp
+++ dp/doubling.cpp
@@ -8,31 +8,55 @@ using namespace std;
 
 #include "../structure/2d_array.cpp"
 
-struct doubling {
+template<typename T = int> struct doubling {
 private:
 	const int n;
 	const long long max_t;
 	v2d<int> table;
+	v2d<T> data;
+
+	function<T(T &, T &)> f = [this](T &l, T &r) { return r; };
 
 public:
 	doubling(const vector<int> &v, long long _max_t) : n(v.size()), max_t(_max_t) {
 		int k = 0;
 		while((1LL << k) <= max_t) k++;
 		table.assign(k, n);
+		data.assign(k, n);
+		table[0] = data[0] = v;
+
+		for(int i = 0; i < k - 1; i++) {
+			for(int j = 0; j < n; j++) {
+				table[i + 1][j] = data[i + 1][j] = table[i][table[i][j]];
+			}
+		}
+	}
+	doubling(const vector<int> &v, const vector<T> &v_data, long long _max_t, function<T(T &, T &)> _f) :
+		n(v.size()), max_t(_max_t), f(_f) {
+		int k = 0;
+		while((1LL << k) <= max_t) k++;
+		table.assign(k, n);
+		data.assign(k, n);
 		table[0] = v;
+		data[0] = v_data;
 
 		for(int i = 0; i < k - 1; i++) {
 			for(int j = 0; j < n; j++) {
 				table[i + 1][j] = table[i][table[i][j]];
+				data[i + 1][j] = f(data[i][j], data[i][table[i][j]]);
 			}
 		}
 	}
 
-	int get(int x, long long t) {
+	int get(int x, T init, long long t) {
 		assert(t <= max_t);
-		int res = x;
+		int id = x;
+		T res = init;
 		for(int k = 0; t; k++) {
-			if(t & 1) res = table[k][res];
+			if(t & 1) {
+				res = f(res, data[k][id]);
+				id = table[k][id];
+			}
 			t >>= 1;
 		}
 		return res;
---
+++ test/yuki_1013.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1013"
+
+#ifndef call_include
+#define call_include
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include "dp/doubling.cpp"
+
+int main() {
+	int N, K;
+	cin >> N >> K;
+	vector<long long> P(N + 1), Pv(N + 1);
+	vector<int> v(N + 1);
+	P[0] = v[0] = 0;
+	for(int i = 1; i <= N; i++) {
+		cin >> P[i];
+		v[i] = (i + P[i] - 1) % N + 1;
+	}
+
+	doubling<long long> db(v, P, K, [](long long l, long long r) { return l + r; });
+	for(int i = 1; i <= N; i++) cout << db.get(i, i, K) << endl;
+}
