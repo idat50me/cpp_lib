7154909f3bb4178fca2fffb886f064bec5574a82
idat50me <59227194+idat50me@users.noreply.github.com>
2021-03-24 18:24:49 +0900

    fix: isprime

--- math/isprime.cpp
+++ math/isprime.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 bool isprime(long long n) {
 	if(n == 2) return true;
-	if(n < 2 || n % 2 == 0) return false;
+	if(n < 2 || n % 2 == 0 || n % 3 == 0 || n % 5 == 0) return false;
 
 	long long i = 7;
 	while(i * i <= n) {
