fc712d59c0334ce4a54696c718d52f875a949d8a
idat50me <59227194+idat50me@users.noreply.github.com>
2021-04-19 01:27:30 +0900

    add matrix pow
    verify してない

--- structure/matrix.cpp
+++ structure/matrix.cpp
@@ -6,29 +6,32 @@
 using namespace std;
 #endif
 
-template<typename T>
-struct matrix {
+template<typename T> struct matrix {
 private:
 	vector<vector<T>> m;
 
 public:
 	matrix() {}
 	matrix(int h, int w) : m(h, vector<T>(w)) {}
+	matrix(int h, int w, T init) : m(h, vector(w, init)) {}
 	matrix(const initializer_list<initializer_list<T>> m_init) : m(m_init.begin(), m_init.end()) {}
 
-	void assign(int h, int w) { m.assign(h, vector<T>(w)); }
+	void assign(int h, int w) {
+		m.assign(h, vector<T>(w));
+	}
 
 	int height() const {
 		return m.size();
 	}
 
 	int width() const {
-		if(height()==0) return 0;
+		if(height() == 0) return 0;
 		return m[0].size();
 	}
 
 	void in() {
-		for(vector<T> &v: m) for(T &val: v) cin>>val;
+		for(vector<T> &v : m)
+			for(T &val : v) cin >> val;
 	}
 	void in(int h, int w) {
 		m.resize(h, vector<T>(w));
@@ -37,55 +40,70 @@ public:
 
 	void out() {
 		int w = width();
-		for(vector<T> &v: m) for(int j=0; j<w; j++) {
-			cout<<v[j]<<(j==w-1 ? '\n' : ' ');
-		}
-		cout<<flush;
+		for(vector<T> &v : m)
+			for(int j = 0; j < w; j++) {
+				cout << v[j] << (j == w - 1 ? '\n' : ' ');
+			}
+		cout << flush;
 	}
 
 	inline const vector<T> &operator[](int idx) const {
-		assert(0<=idx && idx<m.size());
+		assert(0 <= idx && idx < m.size());
 		return m[idx];
 	}
 	inline vector<T> &operator[](int idx) {
-		assert(0<=idx && idx<m.size());
+		assert(0 <= idx && idx < m.size());
 		return m[idx];
 	}
 
 	static matrix identity(int n) {
 		matrix res(n, n, 0);
-		for(int i=0; i<n; i++) res[i][i] = 1;
+		for(int i = 0; i < n; i++) res[i][i] = 1;
 		return res;
 	}
 
-	matrix& operator+=(const matrix& a) {
+	matrix &operator+=(const matrix &a) {
 		int h = height(), w = width();
-		assert(h==a.height() && w==a.width());
-		for(int i=0; i<h; i++) for(int j=0; j<w; j++) m[i][j] += a[i][j];
+		assert(h == a.height() && w == a.width());
+		for(int i = 0; i < h; i++)
+			for(int j = 0; j < w; j++) m[i][j] += a[i][j];
 		return *this;
 	}
-	matrix& operator-=(const matrix& a) {
+	matrix &operator-=(const matrix &a) {
 		int h = height(), w = width();
-		assert(h==a.height() && w==a.width());
-		for(int i=0; i<h; i++) for(int j=0; j<w; j++) m[i][j] -= a[i][j];
+		assert(h == a.height() && w == a.width());
+		for(int i = 0; i < h; i++)
+			for(int j = 0; j < w; j++) m[i][j] -= a[i][j];
 		return *this;
 	}
-	matrix& operator*=(const matrix& a) {
+	matrix &operator*=(const matrix &a) {
 		int h = height(), w = a.width(), ah = a.height();
-		assert(width()==ah);
+		assert(width() == ah);
 		vector<vector<T>> tmp(h, vector(w, T(0)));
-		for(int i=0; i<h; i++) for(int j=0; j<w; j++) for(int k=0; k<ah; k++) tmp[i][j] += m[i][k]*a[k][j];
+		for(int i = 0; i < h; i++)
+			for(int j = 0; j < w; j++)
+				for(int k = 0; k < ah; k++) tmp[i][j] += m[i][k] * a[k][j];
 		m.swap(tmp);
 		return *this;
 	}
 
-	matrix operator+(const matrix& a) const {
-		return matrix(*this)+=a;
+	matrix operator+(const matrix &a) const {
+		return matrix(*this) += a;
 	}
-	matrix operator-(const matrix& a) const {
-		return matrix(*this)-=a;
+	matrix operator-(const matrix &a) const {
+		return matrix(*this) -= a;
 	}
-	matrix operator*(const matrix& a) const {
-		return matrix(*this)*=a;
+	matrix operator*(const matrix &a) const {
+		return matrix(*this) *= a;
+	}
+
+	static matrix pow(matrix a, long long ex) {
+		matrix res = identity(a.height());
+		while(ex > 0) {
+			if(ex & 1) res *= a;
+			ex >>= 1;
+			a *= a;
+		}
+		return res;
 	}
 };
