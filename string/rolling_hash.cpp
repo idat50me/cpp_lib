d5ea42dbcba6177b499d84fb897c2be3507735b5
idat50me <59227194+idat50me@users.noreply.github.com>
2023-10-04 01:52:34 +0900

    update rolling_hash

--- string/rolling_hash.cpp
+++ string/rolling_hash.cpp
@@ -6,39 +6,33 @@
 using namespace std;
 #endif
 
-#include "../math/montgomery.cpp"
-
 struct RollingHash {
 private:
 	const int N;
 	const long long MOD1 = 1000000007;
 	const long long MOD2 = 1992732529;
-	Montgomery mg1 = Montgomery(MOD1);
-	Montgomery mg2 = Montgomery(MOD2);
-	long long base1, base2, base1_mg, base2_mg;
+	random_device rd;
+	mt19937 mt = mt19937(rd());
+	uniform_int_distribution<long long> dist1, dist2;
+	const long long base1, base2;
 	vector<long long> hash1, hash2, pw1, pw2;
 
 public:
 	RollingHash(string &s) :
 		N(s.size()),
+		dist1(257, MOD1 - 2),
+		dist2(257, MOD2 - 2),
+		base1(dist1(mt)),
+		base2(dist2(mt)),
 		hash1(N + 1, 0),
 		hash2(N + 1, 0),
-		pw1(N + 1, mg1.conv_mg(1)),
-		pw2(N + 1, mg2.conv_mg(1)) {
-		random_device rd;
-		mt19937 mt(rd());
-		uniform_int_distribution<long long> dist1(257, MOD1 - 2);
-		uniform_int_distribution<long long> dist2(257, MOD2 - 2);
-		base1 = dist1(mt);
-		base2 = dist2(mt);
-		base1_mg = mg1.conv_mg(base1);
-		base2_mg = mg2.conv_mg(base2);
-
+		pw1(N + 1, 1),
+		pw2(N + 1, 1) {
 		for(int i = 0; i < N; i++) {
-			pw1[i + 1] = mg1.reduction(pw1[i] * base1_mg);
-			pw2[i + 1] = mg2.reduction(pw2[i] * base2_mg);
-			hash1[i + 1] = mg1.reduction(hash1[i] * base1_mg) + mg1.reduction(s[i]);
-			hash2[i + 1] = mg2.reduction(hash2[i] * base2_mg) + mg2.reduction(s[i]);
+			pw1[i + 1] = pw1[i] * base1 % MOD1;
+			pw2[i + 1] = pw2[i] * base2 % MOD2;
+			hash1[i + 1] = hash1[i] * base1 % MOD1 + int(s[i]);
+			hash2[i + 1] = hash2[i] * base2 % MOD2 + int(s[i]);
 			if(hash1[i + 1] >= MOD1) hash1[i + 1] -= MOD1;
 			if(hash2[i + 1] >= MOD2) hash2[i + 1] -= MOD2;
 		}
@@ -47,8 +41,8 @@ public:
 	pair<long long, long long> get(int l, int r) {
 		assert(l <= r);
 		assert(0 <= l and r <= N);
-		long long h1 = hash1[r] - mg1.reduction(hash1[l] * pw1[r - l]);
-		long long h2 = hash2[r] - mg2.reduction(hash2[l] * pw2[r - l]);
+		long long h1 = hash1[r] - hash1[l] * pw1[r - l] % MOD1;
+		long long h2 = hash2[r] - hash2[l] * pw2[r - l] % MOD2;
 		if(h1 < 0) h1 += MOD1;
 		if(h2 < 0) h2 += MOD2;
 		return pair(h1, h2);
@@ -61,8 +55,8 @@ public:
 		vector<int> ret;
 		long long h1_t = 0, h2_t = 0;
 		for(int i = 0; i < M; i++) {
-			h1_t = mg1.reduction(h1_t * base1_mg) + mg1.reduction(t[i]);
-			h2_t = mg2.reduction(h2_t * base2_mg) + mg2.reduction(t[i]);
+			h1_t = h1_t * base1 % MOD1 + int(t[i]);
+			h2_t = h2_t * base2 % MOD2 + int(t[i]);
 			if(h1_t >= MOD1) h1_t -= MOD1;
 			if(h2_t >= MOD2) h2_t -= MOD2;
 		}
