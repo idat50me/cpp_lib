67073db56772facacd46866572b9c0abc2683535
idat50me <59227194+idat50me@users.noreply.github.com>
2023-10-08 11:52:38 +0900

    add aoj_ALDS1_14_B_2.cpp

---
+++ test/aoj_ALDS1_14_B_2.cpp
@@ -0,0 +1,18 @@
+// competitive-verifier: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_14_B
+
+#ifndef call_include
+#define call_include
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+#include "string/rolling_hash.cpp"
+
+int main() {
+	string T, P;
+	cin >> T >> P;
+
+	RollingHash rh(T);
+	vector<int> ans = rh.iscontain(P);
+	for(int &i : ans) cout << i << endl;
+}
